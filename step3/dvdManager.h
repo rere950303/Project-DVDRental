813300c2062074eefb0b34efe95e422306cf4ab6
rere950303 <yhwjjang1995@naver.com>
2021-03-05 20:10:58 +0900

    feat: DvdRental project step3

---
+++ step3/dvdManager.h
@@ -0,0 +1,14 @@
+//
+//  dvdManager.h
+//  step2
+//
+//  Created by hyungwook on 2021/03/05.
+//
+
+#ifndef _DVDMANAGER_H_
+#define _DVDMANAGER_H_
+
+void RegistDvd(void);
+void SearchDvdInfo(void);
+
+#endif
